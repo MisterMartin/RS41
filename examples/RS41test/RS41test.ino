fbc7bb01ab497a001ce6e3eefc78132f819c7a91
MisterMartin <1703358+MisterMartin@users.noreply.github.com>
2024-06-07 22:23:50 -0600

    First working version
    This version polls the RS41 and waits for the returned
    data. The raw string is returned to the caller.
    Changes to be made:
    1. Parse the command return strings and populate a structure which is
       returned to the caller. The structure will have a flag which says if the
       data are valid.
    2. Implement a staggered command/response protocol, where
        the caller initiates a command, and then returns later to get the
        results. This could eliminate the ~18ms wait time (for RSD) which currently
        occurs while waiting for the command response. If the get_sensor_data()
        calls happen at a low enough frequency, we can probably live with the
        18ms dead time.

--- examples/RS41test/RS41test.ino
+++ examples/RS41test/RS41test.ino
@@ -2,13 +2,48 @@
 
 #include <RS41.h>
 
-RS41 myLib; // Create an instance of RS41
+RS41 rs41(Serial7); // Create an instance of RS41
 
-void setup() {
-  myLib.begin();    // Initialize the library
-  myLib.printHello(); // Use the printHello method
+bool recondition = false;
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.print("RS41test build ");
+  Serial.print(__DATE__);
+  Serial.print(" ");
+  Serial.println(__TIME__);
+
+  Serial.println("Do you want to recondition [y/n]? ");
+  while (Serial.available() == 0) {}
+  String ans = Serial.readString().trim();
+  if ((ans=="Y") || (ans=="y")) {
+    recondition = true;
+  }
+
+  rs41.init();
+
 }
 
-void loop() {
-  // Your loop code here
+void loop()
+{
+
+  if (recondition) {
+    String recond = rs41.recondition();
+    if (!recond.length()){
+      Serial.println("RS41 did not respond to RHS");
+    } else {
+      Serial.print("Recondition: ");
+      Serial.println(recond);
+      recondition = false;
+    }
+  }
+  String sensor = rs41.read_sensor_data();
+  if (!sensor.length()){
+    Serial.println("RS41 did not respond to RSD");
+  } else {
+    Serial.print("Sensor: ");
+    Serial.println(sensor);
+  }
+  delay(2000);
 }

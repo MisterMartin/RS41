d7b2441094c98ed60d5d40f0060231cb8f02b2b1
MisterMartin <1703358+MisterMartin@users.noreply.github.com>
2024-06-07 22:23:50 -0600

    First working version
    This version polls the RS41 and waits for the returned
    data. The raw string is returned to the caller.
    Changes to be made:
    1. Parse the command return strings and populate a structure which is
       returned to the caller. The structure will have a flag which says if the
       data are valid.
    2. Implement a staggered command/response protocol, where
        the caller initiates a command, and then returns later to get the
        results. This could eliminate the ~18ms wait time (for RSD) which currently
        occurs while waiting for the command response. If the get_sensor_data()
        calls happen at a low enough frequency, we can probably live with the
        18ms dead time.
    3. In order for this class to be general purpose, we want to
       have the serial port specified in the constructor. On the Teensy,
       there are SerialN ports which have a a addMemoryForWrite() method.
       (Which we want to use). It's not clear what the type is for these
       ports; but they must be derived from HardwareSerial. Currently
       the constructor signature is RS41(HardwareSerial&). Since
       HardwareSerial does not have addMemoryForWrite(), we can't call it.
       Need to find out what the actual Teensy defined type is for SerialN,
       so that we can use this function.

--- src/RS41.cpp
+++ src/RS41.cpp
@@ -2,17 +2,77 @@
 #include "Arduino.h"
 #include "RS41.h"
 
-// Constructor
-RS41::RS41() {
-  _exampleVar = 0;
+RS41::RS41(HardwareSerial& serial):
+_serial(serial) 
+{
 }
 
-// Initialization method
-void RS41::begin() {
-  Serial.begin(9600);
+RS41::~RS41() {
+  digitalWrite(RS41_GPIO_PWR_PIN, LOW);
 }
 
-// Method to print "Hello, World!"
-void RS41::printHello() {
-  Serial.println("Hello, World!");
+void RS41::init() {
+  _serial.begin(56700);
+
+  // Increase serial buffer sizes for Teensy 4.1
+  //_serial.addMemoryForRead(&_rs41_rx_buffer, sizeof(_rs41_rx_buffer));
+  //_serial.addMemoryForWrite(&_rs41_tx_buffer, sizeof(_rs41_tx_buffer));
+
+  // Power cycle the RS41
+  pinMode(RS41_GPIO_PWR_PIN, OUTPUT);
+  digitalWrite(RS41_GPIO_PWR_PIN, LOW);
+  delay(100);
+  digitalWrite(RS41_GPIO_PWR_PIN, HIGH);
+
+  // The RS41 immediately sends out a banner
+  String banner;
+  for (int i = 0; i < 5; i++) {
+      banner = _serial.readStringUntil('\r');
+      if (banner.indexOf("NCAR") != -1) {
+        Serial.print("Banner: ");
+        Serial.println(banner);
+        break;
+    }
+  }
+
+  // Wait for RS41 to get running
+  delay(1000);
+
+  // Get the meta data
+  String meta;
+  for (int i = 0; i < 5; i++) {
+      meta = read_meta_data();
+      if (meta.indexOf(",") != -1) {
+        Serial.print("Meta: ");
+        Serial.println(meta);
+        break;
+    }
+  }
+
+}
+
+String RS41::read_sensor_data() {
+  return(rs41_cmd("RSD"));
+}
+
+String RS41::read_meta_data() {
+  return(rs41_cmd("RMD"));
+}
+
+String RS41::recondition() {
+  return(rs41_cmd("RHS"));
+}
+
+String RS41::rs41_cmd(const String& cmd) {
+  // Note that the default timeout for serial reads is 1000ms.
+  // This is generous for sending a command to the RS41, having
+  // it process the command, and return the value.
+  //
+  // If the flush and read wait times are a problem, we
+  // can implement a scheme where the command is sent,
+  // and we return later to get get the result.
+  _serial.write(cmd.c_str());
+  _serial.write("\r");
+  _serial.flush();
+  return _serial.readStringUntil('\r');
 }
--- src/RS41.h
+++ src/RS41.h
@@ -4,13 +4,61 @@
 
 #include "Arduino.h"
 
+
+struct RS41SensorData {
+};
+
 class RS41 {
+
+#define RS41_SERIAL_BUFFER_SIZE 1024
+#define RS41_GPIO_PWR_PIN 32
+
   public:
-    RS41();              // Constructor
-    void begin();             // Initialization method
-    void printHello();        // Example method to print "Hello, World!"
+    /// @brief Constructor
+    /// @param serial The RS421 serial port
+    RS41(HardwareSerial& serial);
+    /// @brief Destructor
+    ///   Power off RS41
+    ~RS41();
+    /// @brief Initialize RS41
+    /// - Initialize the RS41 serial port
+    /// - Power cycle the RS41
+    /// - Read the banner message
+    /// - Get the RS41 meta data
+    void init();
+    /// @brief Query the RS41 for the RS41 sensor data
+    /// @return A string containing the sensor data. The 
+    /// string will be zero length if the query timed out.
+    String read_sensor_data();
+    /// @brief Query the RS41 for the RS41 meta data
+    /// @return A string containing the sensor data. The 
+    /// string will be zero length if the query timed out.
+    String read_meta_data();
+    /// @brief Start the recondition process
+    /// @return ">RH Reconditioning". A zero length string
+    /// will be returned if the query timed out.
+    String recondition();
+    /// @brief Send the command to the RS41
+    /// @param cmd A RS41 command (.e.g. RMD, RSD, RHS)
+    /// @return The commnd result
+    /// The command is written to RS41_SERIAL, followed by
+    /// a flush(). The flush() will hold until all of
+    /// the characters have been transmitted. This is followed
+    /// by a readStringUntil('\r'). Thus this function will take
+    /// a relatively long time to execute, because it includes the
+    /// time to send the cmd characters, the time for the RS41 to 
+    /// make the measurements, and the time for all of the result 
+    /// characters to be read by the UART. At 56700 baud, and for
+    /// a total of (say) 100 characters to be sent and received,
+    /// this would be ~18ms.
+    String rs41_cmd(const String& cmd);
+
   private:
-    int _exampleVar;          // Example private variable
+    HardwareSerial& _serial;
+    uint8_t _rs41_rx_buffer[RS41_SERIAL_BUFFER_SIZE];
+    uint8_t _rs41_tx_buffer[RS41_SERIAL_BUFFER_SIZE];
+    RS41SensorData _sensor_data;
+
 };
 
 #endif

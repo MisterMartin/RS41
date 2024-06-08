b5d9dc8520a98fcc9a9b53c25a22737253c87cc4
MisterMartin <1703358+MisterMartin@users.noreply.github.com>
2024-06-08 11:42:03 -0600

    Utilize the decoded data features of RS41

--- examples/RS41test/RS41test.ino
+++ examples/RS41test/RS41test.ino
@@ -1,32 +1,42 @@
-// BasicExample.ino
+// Test an RS41 module
+
+// The user is prompted whether the RS41 should be reconditioned.
 
 #include <RS41.h>
 
-RS41 rs41(Serial7); // Create an instance of RS41
-
+bool first_loop = true;
 bool recondition = false;
+RS41 rs41(Serial7);
 
 void setup()
 {
   Serial.begin(115200);
-  Serial.print("RS41test build ");
+  Serial.print("RS41test built: ");
   Serial.print(__DATE__);
-  Serial.print(" ");
+  Serial.print(",");
   Serial.println(__TIME__);
 
-  Serial.println("Do you want to recondition [y/n]? ");
+  Serial.println("Do you want to recondition the RS41[y/n]? ");
   while (Serial.available() == 0) {}
   String ans = Serial.readString().trim();
   if ((ans=="Y") || (ans=="y")) {
     recondition = true;
   }
 
+  // Configure the serial port, power on the RS41, capture the banner, query the metadata.
   rs41.init();
-
 }
 
 void loop()
 {
+  if (first_loop) {
+    Serial.println(rs41.banner());
+    Serial.println(rs41.sensor_data_var_names);
+    first_loop = false;
+  }
+
+  // A delay at the start prevents missing the first data frame
+  delay(2000);
 
   if (recondition) {
     String recond = rs41.recondition();
@@ -38,12 +48,28 @@ void loop()
       recondition = false;
     }
   }
-  String sensor = rs41.read_sensor_data();
-  if (!sensor.length()){
-    Serial.println("RS41 did not respond to RSD");
+
+  RS41::RS41SensorData sensor_data = rs41.decoded_sensor_data();
+  if (sensor_data.valid) {
+    Serial.print(sensor_data.frame_count); Serial.print(",");
+    Serial.print(sensor_data.air_temp_degC); Serial.print(",");
+    Serial.print(sensor_data.humdity_percent); Serial.print(",");
+    Serial.print(sensor_data.hsensor_temp_degC); Serial.print(",");
+    Serial.print(sensor_data.pres_mb); Serial.print(",");
+    Serial.print(sensor_data.internal_temp_degC); Serial.print(",");
+    Serial.print(sensor_data.module_status); Serial.print(",");
+    Serial.print(sensor_data.module_error); Serial.print(",");
+    Serial.print(sensor_data.pcb_supply_V); Serial.print(",");
+    Serial.print(sensor_data.lsm303_temp_degC); Serial.print(",");
+    Serial.print(sensor_data.pcb_heater_on); Serial.print(",");
+    Serial.print(sensor_data.mag_hdgXY_deg); Serial.print(",");
+    Serial.print(sensor_data.mag_hdgXZ_deg); Serial.print(",");
+    Serial.print(sensor_data.mag_hdgYZ_deg); Serial.print(",");
+    Serial.print(sensor_data.accelX_mG); Serial.print(",");
+    Serial.print(sensor_data.accelY_mG); Serial.print(",");
+    Serial.print(sensor_data.accelZ_mG);
+    Serial.println();
   } else {
-    Serial.print("Sensor: ");
-    Serial.println(sensor);
+    Serial.println("Unable to obtain RS41 sensor data");
   }
-  delay(2000);
 }
